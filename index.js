e2a241639d8a266708316c1033ee9ef4f31d5eff
nicolethai <ThaiNicole@gmail.com>
2016-05-12 11:08:28 -0700

    firebase links should be in html and js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+var myDataRef = new Firebase('https://eq4vxp0rje5.firebaseio-demo.com/');
+/*
+    WHY DIS NO SHOW?
+*/
+
 var stickButton = document.getElementById('stick');
 var notes = document.getElementById('notes');
 
 var postIt = function() {
+    
     var note = document.getElementById('note').value;
     var date = document.getElementById('date').value;
     
-    var sticky = note + " stuck since" + date;
+    myDataRef.push({note: note, date: date});
+
+};
+    
+myDataRef.on('child_added', function(data) {
+    
+    var content = data.val();
+    displayNewNote(content.note, content.date);
+    
+    }
+);
+    
+var displayNewNote = function(note, date) {
+    
+    var sticky = note + " stuck since " + date;
     
     var posted = document.createElement("p");
     posted.textContent = sticky;
     notes.appendChild(posted);
     
-}
+};
 
 stickButton.onclick = postIt;
